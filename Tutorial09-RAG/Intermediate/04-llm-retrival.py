af3177353d272f06e8b9411a75aaaab3a534aa82
JA <jatindera@gmail.com>
2024-09-09 10:17:51 +0530

    retrival runnable

--- Tutorial09-RAG/Intermediate/04-llm-retrival.py
+++ Tutorial09-RAG/Intermediate/04-llm-retrival.py
@@ -5,9 +5,20 @@ from langchain_pinecone import PineconeVectorStore
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import Pinecone
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 
 
 load_dotenv()
+
+# See full prompt at https://smith.langchain.com/hub/rlm/rag-prompt
+prompt = hub.pull("rlm/rag-prompt")
+
+def format_docs(docs):
+    x = "\n\n".join(doc.page_content for doc in docs)
+    print(x)
+    return x
 
 index_name = "chruchill-speech"
 embeddings = OpenAIEmbeddings(model="text-embedding-3-small", dimensions=1536)
@@ -15,12 +26,17 @@ llm = ChatOpenAI(model="gpt-4o", max_tokens=200, temperature=0.7)
 
 vector_store = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings)
 # vector_store = PineconeVectorStore(index=index_name, embedding=embeddings)
-# print(vector_store)
-
-retriever = vector_store.as_retriever(search_type = 'similarity', search_kwargs={'k':3})
-
-chain = RetrievalQA.from_chain_type(llm = llm, chain_type='stuff', retriever=retriever)
 
 query = 'Where should we fight?'
-answer = chain.invoke(query)
-print(answer['result'])
+
+qa_chain = (
+    {
+        "context": vector_store.as_retriever() | format_docs,
+        "question": RunnablePassthrough(),
+    }
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+answer = qa_chain.invoke(query)
+print(answer)
